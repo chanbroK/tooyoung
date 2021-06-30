4bd9923e7c030500192f4f9f4b224afabbbf4f2b
BeADia <koc0819@gmail.com>
2021-07-01 00:18:35 +0900

    test react animation library

---
+++ src/App3.tsx
@@ -0,0 +1,21 @@
+import "animate.css/animate.min.css";
+import ScrollAnimation from "react-animate-on-scroll";
+import React from "react";
+import sample from "./sample.jpg";
+
+const sampleSize = "400";
+const App3 = () => {
+  const result = [];
+  for (let i = 0; i < 20; i++) {
+    result.push(
+      <ScrollAnimation animateIn="fadeIn">
+        <h1>SomeSomes</h1>
+        <h2>Some Text</h2>
+        <img src={sample} width={sampleSize} height={sampleSize} alt="sample" />
+      </ScrollAnimation>
+    );
+  }
+  return <div>{result}</div>;
+};
+
+export default App3;
