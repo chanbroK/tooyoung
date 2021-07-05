8db3fe055f0ef069ab5420bbf0720edbd163eac6
BeADia <koc0819@gmail.com>
2021-07-05 22:04:26 +0900

    clean dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,167 +1,21 @@
+import "animate.css/animate.min.css";
+import ScrollAnimation from "react-animate-on-scroll";
 import React from "react";
-import styled from "styled-components";
-import Fade from "react-reveal/Fade"; // Import react-reveal(Fade)
+import sample from "./sample.jpg";
 
-// react reveal 이용
-const IntroBlock = styled.div`
-  margin-bottom: 135px;
-  @media (max-width: 768px) {
-    padding: 25px;
+const sampleSize = "400";
+const App3 = () => {
+  const result = [];
+  for (let i = 0; i < 20; i++) {
+    result.push(
+      <ScrollAnimation animateIn="fadeIn">
+        <h1>SomeSomes</h1>
+        <h2>Some Text</h2>
+        <img src={sample} width={sampleSize} height={sampleSize} alt="sample" />
+      </ScrollAnimation>
+    );
   }
-
-  h1 {
-    margin: 0;
-    font-weight: 300;
-    font-size: 45px;
-    @media (max-width: 768px) {
-      font-size: 30px;
-    }
-  }
-  h3 {
-    font-weight: 200;
-    font-size: 15px;
-  }
-`;
-
-const App = () => {
-  return (
-    <div>
-      <Fade bottom>
-        {/*  위에서 Import한 Fade모듈을 컴포넌트 형태로 감싸주면 영역 내부의
-        태그들만 영향을 받게 된다. 또한 top, bottom 등 방향을 설정해줄 수 있음.
-        */}
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3><h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-
-        </IntroBlock>
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-        </IntroBlock>
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-        </IntroBlock>
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-        </IntroBlock>
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-        </IntroBlock>
-        <IntroBlock>
-          <h1>
-            행사 기획자에서 부터 <br />
-            퍼스널 트레이너, <br />
-            그리고 <strong>개발자</strong>를 꿈꾸기 까지
-          </h1>
-          <h3>
-            Lorem Ipsum is simply dummy text of the <br />
-            printing and typesetting industry. Lorem Ipsum has been <br />
-            the industry's standard dummy text ever since the 1500s
-          </h3>
-        </IntroBlock>
-      </Fade>
-    </div>
-  );
+  return <div>{result}</div>;
 };
 
-export default App;
+export default App3;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import App2 from "./App2";
-import App3 from "./App3";
 import RegistEmail from "./component/RegistEmail";
 import SaveEmail from "./component/SaveEmail";
 // 참고 사이트
