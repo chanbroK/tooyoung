675bb1b53dd288f7148adfd1472f09d95152907e
BeADia <koc0819@gmail.com>
2021-07-06 10:08:05 +0900

    install react-scroll

--- src/Normal.tsx
+++ src/Normal.tsx
@@ -1,8 +1,17 @@
 import App from "./App";
 import FirstPage from "./component/FirstPage";
 import React from "react";
+import * as Scroll from "react-scroll";
 
 const Normal = () => {
+  React.useEffect(() => {
+    Scroll.Events.scrollEvent.register("begin", (to, element) => {
+      console.log("begin scroll");
+    });
+    Scroll.Events.scrollEvent.register("end", (to, element) => {
+      console.log("begin scorll");
+    });
+  }, []);
   return (
     <div>
       <FirstPage />
