c4b3a145199fde83390ba22bb453d47a2793aa77
BeADia <koc0819@gmail.com>
2021-07-06 09:49:21 +0900

    add router

---
+++ src/Normal.tsx
@@ -0,0 +1,14 @@
+import App from "./App";
+import FirstPage from "./component/FirstPage";
+import React from "react";
+
+const Normal = () => {
+  return (
+    <div>
+      <FirstPage />
+      <App />
+    </div>
+  );
+};
+
+export default Normal;
