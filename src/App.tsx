29fdc6113aa462f439e36ff1ac899ac3d4ad2514
BeADia <koc0819@gmail.com>
2021-07-07 14:11:41 +0900

    add AOS to scroll action

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
 import "animate.css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ScrollAnimation from "react-animate-on-scroll";
 import React from "react";
-import sample from "./sample.jpg";
-
 import Navigation from "./component/Nav";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Contact from "./component/Contact";
 
-import RegistEmail from "./component/RegistEmail";
-import SaveEmail from "./component/SaveEmail";
-import { scrollAction } from "./scrollAction";
-const sampleSize = "500";
-
-const App3 = () => {
-  let beforeY = 0;
+const App = () => {
   const result = [];
+  React.useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      debounceDelay: 50,
+      once: false,
+      mirror: true,
+    });
+  });
   for (let i = 0; i < 20; i++) {
     result.push(
-      <ScrollAnimation animateIn="fadeIn">
+      <div data-aos="fade-left" data-aos-easing="ease-in-out">
         <Navigation />
-        <h1>SomeSomes</h1>
-        <h2>Some Text</h2>
-        <img src={sample} width={sampleSize} height={sampleSize} alt="sample" />
-      </ScrollAnimation>
+        <Contact />
+      </div>
     );
   }
-  React.useEffect(() => {
-    window.addEventListener("scroll", () => {
-      scrollAction(500, beforeY);
-      beforeY = window.scrollY;
-    });
-  }, []);
   return <div>{result}</div>;
 };
 
-export default App3;
+export default App;
