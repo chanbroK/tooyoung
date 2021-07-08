5027af5a27f2029a2c6007d1035ef37096f9ba66
BeADia <koc0819@gmail.com>
2021-07-08 09:50:16 +0900

    add up/down scroll move button

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,52 @@ import Navigation from "./component/Nav";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Contact from "./component/Contact";
-
+import sampleImg from "./sample.jpg";
+import { scrollAction } from "./scrollAction";
 const App = () => {
   const result = [];
+  const divRef = React.useRef<HTMLDivElement>();
+
   React.useEffect(() => {
     AOS.init({
       duration: 2000,
       debounceDelay: 50,
       once: false,
-      mirror: true,
+      mirror: false,
     });
+    window.onscroll = () => {
+      scrollAction(divRef.current.clientHeight);
+    };
   });
   for (let i = 0; i < 20; i++) {
     result.push(
-      <div data-aos="fade-left" data-aos-easing="ease-in-out">
-        <Navigation />
-        <Contact />
+      <div
+        data-aos="fade-left"
+        data-aos-easing="ease-in-out"
+        style={{ height: "100vh" }}
+        ref={divRef}
+      >
+        <img src={sampleImg} height="100%" alt="sample" />
+        <button
+          onClick={() => {
+            window.scrollTo(
+              window.scrollX,
+              window.scrollY + divRef.current.clientHeight
+            );
+          }}
+        >
+          down
+        </button>
+        <button
+          onClick={() => {
+            window.scrollTo(
+              window.scrollX,
+              window.scrollY - divRef.current.clientHeight
+            );
+          }}
+        >
+          up
+        </button>
       </div>
     );
   }
--- src/scrollAction.ts
+++ src/scrollAction.ts
@@ -1,15 +1,17 @@
 //https://www.npmjs.com/package/react-scroll
 // 전체 div를 scroll로 감싸서 브라우저의 scroll이 아닌 reactDom의 scroll을 사용해야함
-export const scrollAction = (perWidth: number, beforeY: number) => {
+let beforeY = 0;
+export const scrollAction = (perHeight: number) => {
   const curY = window.scrollY;
-  let newY;
-  if (curY % perWidth === 0) {
+  if (curY % perHeight === 0) {
     return;
   }
   if (curY > beforeY) {
     console.log("down");
+    window.scrollTo(window.scrollX, window.scrollY + perHeight);
   } else {
     console.log("up");
+    window.scrollTo(window.scrollX, window.scrollY - perHeight);
   }
   // if(curY % 500 > 50){
   //   window.scrollTo(scrollX,)
