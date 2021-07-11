36a542f7e2eef1773616a72011748b8163a4237f
BeADia <koc0819@gmail.com>
2021-07-12 08:47:28 +0900

    complete scroll action div

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,76 @@
 import "animate.css/animate.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import Navigation from "./component/Nav";
-import AOS from "aos";
 import "aos/dist/aos.css";
 import Contact from "./component/Contact";
-import sampleImg from "./sample.jpg";
-import { scrollAction } from "./scrollAction";
 import FadeIn from "react-fade-in";
-let beforeY = 0;
 const MAX = 10;
 const MIN = 1;
+
 const App = () => {
   const [curDivId, setCurDivId] = React.useState(1);
   React.useEffect(() => {
-    window.scrollTo(0, 50);
-
-    console.log(window.scrollY);
-    window.onscroll = () => {
-      console.log("ASDF");
-      if (beforeY < window.scrollY) {
-        if (Math.abs(beforeY - window.scrollY) > 100 && curDivId < MAX) {
-          console.log("down");
-          beforeY = window.scrollY;
-          setCurDivId(o => o + 1);
-          window.scrollTo(0, 50);
-          console.log(curDivId);
-        }
-      } else if (beforeY > window.scrollY) {
-        if (Math.abs(beforeY - window.scrollY) > 100 && curDivId > MIN) {
-          console.log("up");
-          beforeY = window.scrollY;
-          setCurDivId(o => o - 1);
-          window.scrollTo(0, 50);
-          console.log(curDivId);
-        }
+    window.onwheel = (event: WheelEvent) => {
+      console.log(event.deltaY);
+      if (event.deltaY > 0 && curDivId < MAX) {
+        console.log("down");
+        setCurDivId((o) => o + 1);
+      } else if (event.deltaY < 0 && curDivId > MIN) {
+        console.log("up");
+        setCurDivId((o) => o - 1);
       }
     };
-  });
-  React.useEffect(() => {});
+  }, [curDivId]);
   return (
     <div>
       {curDivId === 1 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          {/* <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div> */}
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          {/* <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div> */}
         </FadeIn>
       ) : null}
       {curDivId === 2 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 3 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 4 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 5 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 6 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 7 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 8 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 9 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
       {curDivId === 10 ? (
-        <FadeIn transitionDuration={2000} visible={true}>
-          <div style={{ backgroundColor: "blue", width: "100vw", height: "50px" }}></div>
+        <FadeIn transitionDuration={1500} visible={true}>
           <Contact />
-          <div style={{ backgroundColor: "red", width: "100vw", height: "50px" }}></div>
         </FadeIn>
       ) : null}
     </div>
--- src/test.tsx
+++ src/test.tsx
@@ -1,56 +1,64 @@
 import React from "react";
 import ScrollableAnchor from "react-scrollable-anchor";
 import { goToAnchor } from "react-scrollable-anchor";
-import { removeHash } from "react-scrollable-anchor";
+// import { removeHash } from "react-scrollable-anchor";
 import Contact from "./component/Contact";
+/*global event*/
+/*eslint no-restricted-globals: ["error", "event"]*/
+const removeHash = () => {
+  history.replaceState(
+    "",
+    document.title,
+    window.location.pathname + window.location.search
+  );
+};
+const moveSection = (sectionId: String) => {
+  const tag = document.createElement("a");
+  tag.href = `#${sectionId}`;
+  tag.click();
+  removeHash();
+};
+let beforeY = 0;
+const MAX = 2;
+const MIN = 1;
 
 const Test = () => {
-  const [id, setId] = React.useState(1);
+  const [curDivId, setCurDivId] = React.useState(1);
+
   React.useEffect(() => {
-    console.log(id);
-    window.onscroll = () => {
-      setId(id + 1);
-      goToAnchor(`section ${id + 1}`);
-      removeHash();
+    window.onmousewheel = (event: WheelEvent) => {
+      console.log(event.deltaY);
+
+      if (event.deltaY > 0) {
+        console.log("down");
+        setCurDivId((o) => o + 1);
+        console.log(curDivId + 1);
+        moveSection(`section${curDivId + 1}`);
+      } else if (event.deltaY < 0) {
+        console.log("up");
+        setCurDivId((o) => o - 1);
+        moveSection(`section${curDivId + 1}`);
+      }
     };
   });
   return (
-    <>
+    <div>
       <button
         onClick={() => {
-          goToAnchor("section 2", false);
+          moveSection("section2");
+          const section = document.querySelector("#section2");
+          section.className = "fade";
         }}
       >
         click
       </button>
-      <ScrollableAnchor id={"section 1"}>
+      <section id={"section1"}>
         <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={"section 2"}>
+      </section>
+      <section id={"section2"}>
         <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={3}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={4}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={5}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={6}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={7}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={8}>
-        <Contact />
-      </ScrollableAnchor>
-      <ScrollableAnchor id={9}>
-        <Contact />
-      </ScrollableAnchor>
-    </>
+      </section>
+    </div>
   );
 };
 
