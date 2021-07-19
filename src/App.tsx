b34725aa11366bcb2eac7346dd95c4c4e2cbdf85
jangjaeko <jangjaeko45@gmail.com>
2021-07-19 17:28:15 +0900

    edit title

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import SecondPage from "./component/SecondPage";
 import ThirdPage from "./component/ThirdPage";
 import { Last } from "react-bootstrap/esm/PageItem";
 import LastPage from "./component/LastPage";
+import { Helmet } from "react-helmet";
+import logoImage from "./component/images/mainlogo.jpeg";
 const MAX = 10;
 const MIN = 1;
 
@@ -26,59 +28,67 @@ const App = () => {
       }
     };
   }, [curDivId]);
+  const webTitle = "TooYoung";
   return (
-    <div style={{ fontFamily: "NanumBarumGothic" }}>
-      {curDivId === 1 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <FirstPage />
-        </FadeIn>
-      ) : null}
-      {curDivId === 2 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <SecondPage />
-        </FadeIn>
-      ) : null}
-      {curDivId === 3 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <ThirdPage />
-        </FadeIn>
-      ) : null}
-      {curDivId === 4 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-      {curDivId === 5 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <LastPage />
-        </FadeIn>
-      ) : null}
-      {curDivId === 6 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-      {curDivId === 7 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-      {curDivId === 8 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-      {curDivId === 9 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-      {curDivId === 10 ? (
-        <FadeIn transitionDuration={1500} visible={true}>
-          <Contact />
-        </FadeIn>
-      ) : null}
-    </div>
+    <>
+      <Helmet>
+        <title>TooYoung</title>
+        <meta name="ABC" content="ABC" />
+        <link rel="icon" type="image/jpeg" href={logoImage} sizes="16x16" />
+      </Helmet>
+      <div style={{ fontFamily: "NanumBarumGothic" }}>
+        {curDivId === 1 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <FirstPage />
+          </FadeIn>
+        ) : null}
+        {curDivId === 2 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <SecondPage />
+          </FadeIn>
+        ) : null}
+        {curDivId === 3 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <ThirdPage />
+          </FadeIn>
+        ) : null}
+        {curDivId === 4 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+        {curDivId === 5 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <LastPage />
+          </FadeIn>
+        ) : null}
+        {curDivId === 6 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+        {curDivId === 7 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+        {curDivId === 8 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+        {curDivId === 9 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+        {curDivId === 10 ? (
+          <FadeIn transitionDuration={1500} visible={true}>
+            <Contact />
+          </FadeIn>
+        ) : null}
+      </div>
+    </>
   );
 };
 
