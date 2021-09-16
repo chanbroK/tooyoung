bb6749d4c5f709af7148213383d68052eaf80c98
jangjaeko <jangjaeko45@gmail.com>
2021-09-16 18:47:32 +0900

    make prodcuct component

--- src/newComponent/main/Main.tsx
+++ src/newComponent/main/Main.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import NavB from "../NavBar/NavB";
 import Banner from "./Banner/Banner";
 import HashSearch from "./Search/HashSearch";
 import Product from "./Product/ProductCard";
 import ScrollContainer from "react-indiana-drag-scroll";
-
+import Data from "../Product/Product";
 export default function Main() {
+  const [shoes, setShoes] = useState(Data);
   return (
     <ScrollContainer className="scroll-container">
       <div>
@@ -26,8 +27,8 @@ export default function Main() {
             </div>
           </div>
           <div>
-            <Product />
-            <Product />
+            <Product shoes={shoes} />
+            <Product shoes={shoes} />
           </div>
         </div>
       </div>
--- src/newComponent/main/Product/ProductCard.js
+++ src/newComponent/main/Product/ProductCard.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import facelogo from "../../../component/images/얼굴로고.png";
-export default function ProductCard() {
+export default function ProductCard(props) {
   return (
     <div className="container" style={{ marginTop: "5%" }}>
       <div className="row">
@@ -10,9 +10,15 @@ export default function ProductCard() {
           <p>상품정보</p>
         </div>
         <div className="col-md-3">
-          <img src={facelogo} width="100%" />
-          <h4>상품명</h4>
-          <p>상품정보</p>
+          <img
+            src={props.shoes[0].img}
+            width="100%"
+            style={{ marginBottom: "5%" }}
+          />
+          <h4>상품명 : {props.shoes[0].title}</h4>
+          <p>
+            {props.shoes[0].content} & {props.shoes[0].price}
+          </p>
         </div>
         <div className="col-md-3">
           <img src={facelogo} width="100%" />
